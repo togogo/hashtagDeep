1dc14ff23424666f94b142e9eec0cae455675fbb
the-wangest <rjgwang@gmail.com>
2019-03-28 15:16:52 -0400

    Initial Commit

---
+++ unity/Assets/somescript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Intel.RealSense;
+using UnityEngine.Rendering;
+using UnityEngine.Assertions;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+public class somescript : MonoBehaviour
+{
+    public RsFrameProvider Source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(testM)
+    }
+}
